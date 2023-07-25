294463499de30e60d5e40fb39c716c393ce7cd60
Javier Fadel <javierfadel1208@gmail.com>
2023-07-25 11:28:37 +0700

    Feature fixed.

--- database/seeders/ShoeSeeder.php
+++ database/seeders/ShoeSeeder.php
@@ -6,20 +6,25 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ProductSeeder extends Seeder
+class ShoeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-
     public function run(): void
     {
-        for ($i=0; $i < 10; $i++) { 
+        for ($i=1; $i < 8; $i++) { 
             DB::table('product')->insert([
                 'name' => 'Product ' . $i,
                 'price' =>  floor(mt_rand(40000, 200000)),
-                'image' => 'resources/image/product-' . $i . '.jpg'
-            ]);   
+                'image' => 'resources/image/shoes-men-' . $i . '.jpg'
+            ]);
+
+            DB::table('product')->insert([
+                'name' => 'Product ' . $i,
+                'price' =>  floor(mt_rand(40000, 200000)),
+                'image' => 'resources/image/shoes-women-' . $i . '.jpg'
+            ]);
         }
     }
 }
